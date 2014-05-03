9da324516b729f10c34178f7af0156005b0f2b3d
Brad Jasper <bjasper@gmail.com>
2014-05-03 16:29:13 -0400

    Got initial animation working

--- POP Experiments/ViewController.m
+++ POP Experiments/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
+#import <POP/POP.h>
+
 @interface ViewController ()
+
+@property (nonatomic, strong) UILabel *label;
 
 @end
 
@@ -18,7 +22,35 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.label = [[UILabel alloc] initWithFrame:CGRectMake(50, 50, 100, 100)];
+    self.label.userInteractionEnabled = YES;
+    self.label.text = @"Hello POP!";
+    [self.view addSubview:self.label];
+    
+    NSLog(@"Adding label");
+    
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tappedLabel)];
+    
+    [self.label addGestureRecognizer:tapGesture];
 }
+
+- (void)tappedLabel
+{
+    NSLog(@"Received gesture tap");
+    
+    POPSpringAnimation *springAnim = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
+    
+    springAnim.toValue = @700;
+    springAnim.velocity = @10;
+    springAnim.completionBlock = ^(POPAnimation *anim, BOOL finished) {
+        NSLog(@"Animation is complete");
+        self.label.frame = CGRectMake(50, 50, 100, 100);
+    };
+   
+    [self.label pop_addAnimation:springAnim forKey:@"size"];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
