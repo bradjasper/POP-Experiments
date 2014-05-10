e24fa295c53b826c67b294b0cb7d710f25b8f620
Brad Jasper <bjasper@gmail.com>
2014-05-10 18:19:36 -0400

    got tweaks added

--- POP Experiments/ViewController.m
+++ POP Experiments/ViewController.m
@@ -10,6 +10,9 @@
 
 #import <POP/POP.h>
 
+#import <Tweaks/FBTweak.h>
+#import <Tweaks/FBTweakInline.h>
+
 @interface ViewController ()
 
 @property (nonatomic, strong) UIView *circle;
@@ -25,15 +28,10 @@
     self.view.multipleTouchEnabled = YES;
     
     self.circle = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
-    self.circle.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+    self.circle.center = self.view.center;
     self.circle.layer.cornerRadius = 50;
     self.circle.backgroundColor = [UIColor blueColor];
     [self.view addSubview:self.circle];
-}
-
-- (NSUInteger)supportedInterfaceOrientations
-{
-    return UIInterfaceOrientationMaskPortrait;
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
@@ -57,15 +55,14 @@
 - (CGPoint)getCenterFromTouches:(NSSet *)touches
 {
     int numTouches = [touches count];
-    
     if (numTouches == 0) {
-        return CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+        return self.view.center;
     }
     
     float posX = 0.0, posY = 0.0;
     CGPoint pos;
     
-    for (UITouch *touch in [touches objectEnumerator]) {
+    for (UITouch *touch in touches) {
         pos = [touch locationInView:self.view];
         posX += pos.x;
         posY += pos.y;
@@ -80,13 +77,23 @@
     
     POPSpringAnimation *springAnim = [self.circle pop_animationForKey:@"position"];
     
+    // If animation exists we can just update the toValue...where the magic happens
     if (springAnim != NULL) {
         springAnim.toValue = toValue;
     } else {
+        
+        // Otherwise let's create a new animation using customizable tweaks (shake the ipad/iphone)
+        
+        FBTweak *springSpeedTweak = FBTweakInline(@"Circle", @"Spring", @"Speed", 12, 0, 20);
+        springSpeedTweak.stepValue = @1;
+        
+        FBTweak *springBouncinessTweak = FBTweakInline(@"Circle", @"Spring", @"Bounciness", 10, 0, 20);
+        springBouncinessTweak.stepValue = @1;
+        
         springAnim = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPosition];
         springAnim.toValue = toValue;
-        springAnim.springSpeed = 20;
-        springAnim.springBounciness = 10;
+        springAnim.springSpeed = [[springSpeedTweak currentValue] floatValue];
+        springAnim.springBounciness = [[springBouncinessTweak currentValue] floatValue];
         [self.circle pop_addAnimation:springAnim forKey:@"position"];
     }
 }
@@ -94,7 +101,16 @@
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskPortrait;
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
 }
 
 @end
