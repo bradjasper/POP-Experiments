f4517f9232e435de46eafc73c4f3a393b5ca3ce7
Brad Jasper <bjasper@gmail.com>
2014-05-10 18:58:04 -0400

    Got size working

--- POP Experiments/ViewController.m
+++ POP Experiments/ViewController.m
@@ -32,11 +32,15 @@
     self.circle.layer.cornerRadius = 50;
     self.circle.backgroundColor = [UIColor blueColor];
     [self.view addSubview:self.circle];
+    
+    // Init with empty set of touches ...hacky fix
+    [self updateCircleSize:[self getSizeFromTouches:[NSSet setWithObjects:nil]]];
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     [self updateCircleLocation:[self getCenterFromTouches:[event allTouches]]];
+    [self updateCircleSize:[self getSizeFromTouches:[event allTouches]]];
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
@@ -49,9 +53,21 @@
     NSMutableSet *activeTouches = [[event allTouches] mutableCopy];
     [activeTouches minusSet:touches];
     [self updateCircleLocation:[self getCenterFromTouches:activeTouches]];
+    
+    [self updateCircleSize:[self getSizeFromTouches:activeTouches]];
 }
 
+// Get the size of a circle by the number of touches
+- (CGSize)getSizeFromTouches:(NSSet *)touches
+{
+    int numTouches = [touches count];
+    float initialSize = FBTweakValue(@"Circle", @"Size", @"Initial", 1.0);
+    float percentToGrowPerTouch = FBTweakValue(@"Circle", @"Size", @"PerTouch", 0.1, 0, 5);
+    float radius = initialSize + (numTouches * percentToGrowPerTouch);
+    return CGSizeMake(radius, radius);
+}
 
+// Average a bunch of touches to find the center
 - (CGPoint)getCenterFromTouches:(NSSet *)touches
 {
     int numTouches = [touches count];
@@ -73,28 +89,64 @@
 
 - (void)updateCircleLocation:(CGPoint)location
 {
+    if (!FBTweakValue(@"Circle", @"Position", @"Enable", YES)) {
+        return;
+    }
+    
     NSValue *toValue = [NSValue valueWithCGPoint:location];
     
-    POPSpringAnimation *springAnim = [self.circle pop_animationForKey:@"position"];
+    POPSpringAnimation *anim = [self.circle pop_animationForKey:@"position"];
     
     // If animation exists we can just update the toValue...where the magic happens
-    if (springAnim != NULL) {
-        springAnim.toValue = toValue;
+    if (anim != NULL) {
+        anim.toValue = toValue;
     } else {
         
         // Otherwise let's create a new animation using customizable tweaks (shake the ipad/iphone)
         
-        FBTweak *springSpeedTweak = FBTweakInline(@"Circle", @"Spring", @"Speed", 12, 0, 20);
+        FBTweak *springSpeedTweak = FBTweakInline(@"Circle", @"Position", @"Speed", 12, 0, 20);
         springSpeedTweak.stepValue = @1;
         
-        FBTweak *springBouncinessTweak = FBTweakInline(@"Circle", @"Spring", @"Bounciness", 10, 0, 20);
+        FBTweak *springBouncinessTweak = FBTweakInline(@"Circle", @"Position", @"Bounciness", 10, 0, 20);
         springBouncinessTweak.stepValue = @1;
         
-        springAnim = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPosition];
-        springAnim.toValue = toValue;
-        springAnim.springSpeed = [[springSpeedTweak currentValue] floatValue];
-        springAnim.springBounciness = [[springBouncinessTweak currentValue] floatValue];
-        [self.circle pop_addAnimation:springAnim forKey:@"position"];
+        anim = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPosition];
+        anim.toValue = toValue;
+        anim.springSpeed = [[springSpeedTweak currentValue] floatValue];
+        anim.springBounciness = [[springBouncinessTweak currentValue] floatValue];
+        [self.circle pop_addAnimation:anim forKey:@"position"];
+    }
+}
+
+- (void)updateCircleSize:(CGSize)size
+{
+    if (!FBTweakValue(@"Circle", @"Size", @"Enable", NO)) {
+        return;
+    }
+    
+    NSValue *toValue = [NSValue valueWithCGSize:size];
+    
+    NSLog(@"Updating circle size = %@", NSStringFromCGSize(size));
+    
+    POPSpringAnimation *anim = [self.circle pop_animationForKey:@"scale"];
+    
+    
+    if (anim != NULL) {
+        anim.toValue = toValue;
+    } else {
+        
+        FBTweak *springSpeedTweak = FBTweakInline(@"Circle", @"Size", @"Speed", 18, 0, 20);
+        springSpeedTweak.stepValue = @1;
+        
+        FBTweak *springBouncinessTweak = FBTweakInline(@"Circle", @"Size", @"Bounciness", 4, 0, 20);
+        springBouncinessTweak.stepValue = @1;
+        
+        anim = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+        anim.fromValue = [NSValue valueWithCGPoint:CGPointMake(1, 1)];
+        anim.toValue = toValue;
+        anim.springSpeed = [[springSpeedTweak currentValue] floatValue];
+        anim.springBounciness = [[springBouncinessTweak currentValue] floatValue];
+        [self.circle pop_addAnimation:anim forKey:@"scale"];
     }
 }
 
